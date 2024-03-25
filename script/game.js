ee6fd7ae9f012f8e6ec483cec538abb557934b0d
Suthera213 <64030213@kmitl.ac.th>
2024-03-25 21:27:06 +0700

    update

--- script/game.js
+++ script/game.js
@@ -1,18 +1,15 @@
-const words = ["สัตว์", "ประเทศ", "อาหาร", "สี", "อาชีพ"];
-const clues = ["มีสี่ขา", "อยู่ในทวีปเอเชีย", "มีรสหวาน", "เป็นสีแดง", "รักษาผู้ป่วย"];
-let currentWord = words[Math.floor(Math.random() * words.length)];
-let currentClue = clues[words.indexOf(currentWord)];
-
-function checkGuess() {
-  const guess = document.getElementById("guess").value;
-  const message = document.getElementById("message");
-
-  if (guess === currentWord) {
-    message.textContent = " คุณทายถูก! ";
-    currentWord = words[Math.floor(Math.random() * words.length)];
-    currentClue = clues[words.indexOf(currentWord)];
-    document.getElementById("clue").textContent = currentClue;
-  } else {
-    message.textContent = " คิดอีกนิด! ";
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+function jump(){
+    if (character.classList != "animate") {
+      character.classList.add("animate");
+    }
+    setTimeout(function(){
+      character.classList.remove("animate");
+    },500);
   }
-}
+    var checkDead = setInterval(function(){
+      var characterTop = 
+      parseInt(window.getComputedStyle(character).
+      getPropertyValue("top"));
+    },10);
